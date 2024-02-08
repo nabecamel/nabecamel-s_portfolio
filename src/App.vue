fc898410111ad3b76f228fbc88f1f090d3f7c3b3
watawata4D <eh210577@meiji.ac.jp>
2024-02-08 16:52:24 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="my_photo" src="./assets/IMG_9393 2.png">
+  <GreetingsMessage msg="Welcome to my portfolio site"/>
+</template>
+
+<script>
+import GreetingsMessage from './components/Greeting.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GreetingsMessage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  border-radius: 50%; /* 画像を円形にする */
+  width: 100px; /* 画像の幅を100pxに設定 */
+  height: 100px; /* 画像の高さを100pxに設定 */
+  object-fit: cover; /* 画像の比率を保ちつつ、設定した領域に収める */
+}
+</style>
---
+++ src/components/Greeting.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="portfolio">
+    <h1>{{ msg }}</h1>
+    <p>明治大学情報コミュニケーション学部情報コミュニケーション学科3年の渡辺 航です。</p>
+    <p>さまざまな技術領域に興味を持ち日々学習をしています。</p>
+    
+    <h3>連絡先</h3>
+    <p>Email: watanabe.wataru.2025@gmail.com</p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/%E8%88%AA-%E6%B8%A1%E8%BE%BA-ba8709281/m" target="_blank" rel="noopener">私のLinkedIn</a></p>
+    <p>GitHub: <a href="https://github.com/nabecamel" target="_blank" rel="noopener">私のGitHub</a></p>
+    
+    <h3>スキルセット</h3>
+    <ul>
+      <li>HTML & CSS</li>
+      <li>JavaScript (ES6+)</li>
+      <li>Python</li>
+      <li>C/C++</li>
+      <li>Java</li>
+      <li>R</li>
+      <li>Vue.js</li>
+      <li>PHP</li>
+      <li>Node.js</li>
+      <li>ShellScript</li>
+      <li>laravel</li>
+
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioIntro',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 20px;
+}
+ul {
+  list-style-type: circle;
+  padding-left: 20px;
+}
+a {
+  color: #42b983;
+}
+</style>
